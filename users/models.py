b903b456add3849e8e71475cabef048bc14cc5c3
Nathi33 <nathi33.drnt@outlook.com>
2025-05-05 10:53:26 +0200

    add model-users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_client = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
