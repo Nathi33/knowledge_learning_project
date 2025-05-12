7f6b0ed2d52b16010acae6dba398829fa9bb6010
Nathi33 <nathi33.drnt@outlook.com>
2025-05-12 19:23:32 +0200

    add envoi-mail

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']  # Champs à afficher dans le formulaire de connexion   
+    email = forms.EmailField(required=True, label="Adresse e-mail : ")  
+    last_name = forms.CharField(max_length=30, required=True, label="Nom : ")
+    first_name = forms.CharField(max_length=30, required=True, label="Prénom : ") 
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)  
-        for fieldname in ['username']:
-            self.fields[fieldname].help_text = ''  # Supprime le texte d'aide par défaut des champs  
+    class Meta:
+        model = CustomUser
+        fields = ['last_name', 'first_name', 'email', 'password1', 'password2']  # Champs à afficher dans le formulaire de connexion   
+
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.last_name = self.cleaned_data['last_name']
+        user.first_name = self.cleaned_data['first_name']
+        if commit:
+            user.save()
+        return user
+    
+User = get_user_model()
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(label="Adresse e-mail : ", widget=forms.EmailInput(attrs={'autofocus': True}))
    
+    def confirm_login_allowed(self, user):
+        if not user.is_active:
+            raise forms.ValidationError(
+                "Ce compte n'est pas actif. Veuillez vérifier votre adresse e-mail pour l'activer.",
+                code='inactive',
+            )
--- users/models.py
+++ users/models.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class CustomUser(AbstractUser):
+    username = None  # Supprimer le champ username
+    email = models.EmailField(unique=True, verbose_name=_("Adresse e-mail"))
+    last_name = models.CharField(max_length=30, verbose_name=_("Nom"))
+    first_name = models.CharField(max_length=30, verbose_name=_("Prénom"))
     is_client = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        verbose_name = 'Utilisateur'
+        verbose_name_plural = 'Utilisateurs'
+        ordering = ['email']
