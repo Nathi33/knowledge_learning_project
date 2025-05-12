bb5a92ae9aca953ef0dc16b8e714b03dddbf78d6
Nathi33 <nathi33.drnt@outlook.com>
2025-05-12 20:34:08 +0200

    add admin

--- users/models.py
+++ users/models.py
@@ -1,6 +1,32 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("L'adresse e-mail doit être fournie")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError("Le superutilisateur doit avoir is_staff=True.")
+        if not extra_fields.get('is_superuser'):
+            raise ValueError("Le superutilisateur doit avoir is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -11,6 +37,8 @@ class CustomUser(AbstractUser):
     is_client = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
