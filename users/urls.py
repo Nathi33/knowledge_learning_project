8b053bff45e64cf5fa71b9cc88a6323861039a31
Nathi33 <nathi33.drnt@outlook.com>
2025-05-26 19:30:21 +0200

    add modifs 8

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import register_view, CustomLoginView, activate, CustomLogoutView
-from . import views
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),  
     path('register/', register_view, name='register'),  
     path('logout/', CustomLogoutView.as_view(), name='logout'),   
     path('activate/<uidb64>/<token>/', activate, name='activate'),  
-    path('go/activation/<code>/', views.activation_redirect, name='activation_redirect'),  
 ]
